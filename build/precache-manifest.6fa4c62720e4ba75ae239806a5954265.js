8891f5b613e8c72e74a3a801430dcca0b893eae9
RCR <contreras06rj@gmail.com>
2020-11-07 17:37:23 -0400

    nueva pagina

---
+++ build/precache-manifest.6fa4c62720e4ba75ae239806a5954265.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4a1cd2a251cb1e07025065279e825c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad06b9137564b6b6abb4",
+    "url": "/static/css/main.5925673f.chunk.css"
+  },
+  {
+    "revision": "f7c6d0cdce3cfce4b63b",
+    "url": "/static/js/2.a25ed072.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a25ed072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad06b9137564b6b6abb4",
+    "url": "/static/js/main.4fd55031.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  },
+  {
+    "revision": "5e1e01522b5ec0ef0638185f944e1719",
+    "url": "/static/media/zenior-logo-blanco.5e1e0152.png"
+  }
+]);
